a77634b53eb847bccc56f55efedb64bda30368ae
Nikhil Pai <nikhilpai2016@u.northwestern.edu>
2016-10-23 21:48:20 -0700

    bearling logic

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,56 @@
 $(document).ready(function() {
+	// lib
+	function bearing(lat1,lng1,lat2,lng2) {
+        var dLon = (lng2-lng1);
+        var y = Math.sin(dLon) * Math.cos(lat2);
+        var x = Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
+        var brng = this._toDeg(Math.atan2(y, x));
+        return 360 - ((brng + 360) % 360);
+    };
+
+	function _toRad(deg) {
+         return deg * Math.PI / 180;
+    };
+
+    function _toDeg(rad) {
+        return rad * 180 / Math.PI;
+    };
+
+    // Boot
     Compass.init(function (method) {
 	  console.log('Compass heading by ' + method);
 	});
 
-	Compass.watch(function (heading) {
-	  $('.degrees').text(heading);
-	  $('.compass').css('transform', 'rotate(' + (-heading) + 'deg)');
-	});
+	function getLocation() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.watchPosition(setCompass);
+	    } else {
+	        console.log("Geolocation is not supported by this browser.");
+	    }
+	}
+
+	var target_lat = 37.762223;
+	var target_long = -122.421511;
+
+	//Refresh
+	var watchID;
+
+	function setCompass(position) {
+		var current_lat = position.coords.latitude;
+		var current_long = position.coords.longitude;
+
+		console.log(current_lat)
+		var bear = bearing(current_lat, current_long, target_lat,target_long)
+
+		Compass.unwatch(watchID);
+		watchID = Compass.watch(function (heading) {
+		  $('.degrees').text(heading);
+		  $('.compass').css('transform', 'rotate(' + (-heading +bear) + 'deg)');
+		});
+	}
+
+	//RUN
+	getLocation();
 });
+
+
